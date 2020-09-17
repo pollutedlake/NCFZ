3c223248f61798cd5bf364143ebe3aa967f17295
Pollutedlake <psh950827@naver.com>
2020-09-18 03:27:56 +0900

    create Buttons & create BattleManager & create battlePhase(choiiceButton) & playerMove in choiceButton

---
+++ NCFZ/Assets/Scripts/BattleManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    /// <summary>
+    /// 전투 페이즈
+    /// choiceButton : Player이 fight, act, item, mercy 중 버튼을 고르는 페이즈
+    /// </summary>
+    public enum Phase
+    {
+        choiceButton
+    }
+
+    public Phase battlePhase;        // 전투 페이즈
+
+    /// <summary>
+    /// 전투 시작 시 변수 초기화 작업
+    /// 전투 페이즈는 choiceButton, 
+    /// </summary>
+    void Start()
+    {
+        battlePhase = Phase.choiceButton;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ NCFZ/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public BattleManager battleManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (battleManager.battlePhase)      // 전투페이즈마다 행동이 다르다.
+        {
+            // choiceButton일 때는 양옆으로만 이동하며 이동 시 옆 버튼으로 이동하며 해당 버튼이 활성화 된다.
+            case BattleManager.Phase.choiceButton:
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    transform.position -= new Vector3(4.5f, 0, 0);      // 버튼 간의 간격 만큼 왼쪽으로 이동
+                }
+                else if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    transform.position += new Vector3(4.5f, 0, 0);      // 버튼 간의 간격만큼 오른쪽으로 이동
+                }
+                else if (Input.GetKeyDown(KeyCode.Z))       // 버튼 선택
+                {
+
+                }
+                break;
+        }
+    }
+}
